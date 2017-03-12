0febfacb5c93208a869ebdde0a4432cfb64a5079
Nikul Ukani <nikul@ee.columbia.edu>
2017-03-11 21:56:34 -0500

    Change default port in config to 8081 and remove langid from Dockerfile

--- nlp_component/config.py
+++ nlp_component/config.py
@@ -2,11 +2,11 @@ user = 'component'
 secret = 'xKzKbt62w3vx'
 
 ssl = False
-url = 'ws://localhost:8080/ws'
+url = 'ws://localhost:8081/ws'
 realm = u'realm1'
 authentication = True
 
-debug =False
+debug = False
 
 ca_cert_file = 'isrgrootx1.pem'
 intermediate_cert_file = 'letsencryptauthorityx3.pem'
