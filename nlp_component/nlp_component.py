073382d0d74f9a3503b08dc4fb804b333cf2a3ac
Nikul Ukani <nikul@ee.columbia.edu>
2017-03-11 22:14:55 -0500

    Remove extra code from nlp_component

--- nlp_component/nlp_component.py
+++ nlp_component/nlp_component.py
@@ -61,18 +61,9 @@ class AppSession(ApplicationSession):
         
         translators = {}
         
-        try:
-
-            f = open(os.path.join(os.path.dirname(__file__),'config.json'))
-            with f as data_file:
-                self.server_config = json.load(data_file)
-                self.log.info("Loading Config Dictionary with values, {items}",items=self.server_config.items())
-                f.close()
-
-        except Exception as e:
-            self.server_config = {'name': 'nlp_server'}
-            self.log.warn("Loading Config Dictionary Failed, defaulting to {items}. Exception {e}",
-                          items=self.server_config.items(),e=e)
+        self.server_config = {'name': 'nlp_server'}
+        self.log.warn("Loading Config Dictionary Failed, defaulting to {items}. Exception {e}",
+                      items=self.server_config.items(),e=e)
 
 
         #@inlineCallbacks
@@ -149,7 +140,7 @@ if __name__ == '__main__':
     else:
         txaio.start_logging(level='info')
 
-   # any extra info we want to forward to our ClientSession (in self.config.extra)
+    # any extra info we want to forward to our ClientSession (in self.config.extra)
     extra = {'auth': args.authentication}
 
     if args.ssl:
@@ -171,112 +162,4 @@ if __name__ == '__main__':
         runner = ApplicationRunner(url=args.url, realm=args.realm, extra=extra)
 
     runner.run(AppSession)
-
-
-    from twisted.internet._sslverify import OpenSSLCertificateAuthorities
-    from twisted.internet.ssl import CertificateOptions
-    import OpenSSL.crypto
-
-   
-    # Crossbar.io connection configuration
-    url = os.environ.get('CBURL', u'ws://localhost:8080/ws')
-    realm = os.environ.get('CBREALM', u'realm1')
-
-    auth = True
-
-    # parse command line parameters
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--debug', action='store_true', help='Enable debug output.')
-    parser.add_argument('--url', dest='url', type=six.text_type, default=url, help='The router URL (default: "ws://localhost:8080/ws").')
-    parser.add_argument('--realm', dest='realm', type=six.text_type, default=realm, help='The realm to join (default: "realm1").')
-    parser.add_argument('--no-auth', dest='auth', action='store_false')
-    parser.set_defaults(auth=True)
- 
-    args = parser.parse_args()
-
-    # start logging
-    if args.debug:
-        txaio.start_logging(level='debug')
-    else:
-        txaio.start_logging(level='info')
-
-   # any extra info we want to forward to our ClientSession (in self.config.extra)
-    extra = {
-        u'auth': args.auth
-    }
-
-    if args.auth:    
-        st_cert=open('isrgrootx1.pem', 'rt').read()
-        c=OpenSSL.crypto
-        ca_cert=c.load_certificate(c.FILETYPE_PEM, st_cert)
-
-        st_cert=open('letsencryptauthorityx3.pem', 'rt').read()
-        intermediate_cert=c.load_certificate(c.FILETYPE_PEM, st_cert)
-
-        certs = OpenSSLCertificateAuthorities([ca_cert, intermediate_cert])
-        ssl_con = CertificateOptions(trustRoot=certs)
- 
-        # now actually run a WAMP client using our session class ClientSession
-        runner = ApplicationRunner(url=args.url, realm=args.realm, extra=extra, ssl=ssl_con)
     
-    else:
-        runner = ApplicationRunner(url=args.url, realm=args.realm, extra=extra)
-
-    runner.run(AppSession)
-    from twisted.internet._sslverify import OpenSSLCertificateAuthorities
-    from twisted.internet.ssl import CertificateOptions
-    import OpenSSL.crypto
-    
-                                           
-    
-    # parse command line parameters
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--debug', action='store_true', help='Enable debug output.')
-    parser.add_argument('--url', dest='url', type=six.text_type, default=url,
-                        help='The router URL (defaults to value from config.py)')
-    parser.add_argument('--realm', dest='realm', type=six.text_type, default=realm,
-                        help='The realm to join (defaults to value from config.py).')
-    parser.add_argument('--ca_cert', dest='ca_cert_file', type=six.text_type,
-                        default=ca_cert_file,
-                        help='Root CA PEM certificate file (defaults to value from config.py).')
-    parser.add_argument('--int_cert', dest='intermediate_cert_file', type=six.text_type,
-                        default=intermediate_cert_file,
-                        help='Intermediate PEM certificate file (defaults to value from config.py).')
-    parser.add_argument('--no-ssl', dest='ssl', action='store_false')
-    parser.add_argument('--no-auth', dest='authentication', action='store_false')
-    parser.set_defaults(ssl=ssl)
-    parser.set_defaults(authentication=authentication)
-    parser.set_defaults(debug=debug)
-    
-    args = parser.parse_args()
-
-    
-    # start logging
-    if args.debug:
-        txaio.start_logging(level='debug')
-    else:
-        txaio.start_logging(level='info')
-
-   # any extra info we want to forward to our ClientSession (in self.config.extra)
-    extra = {'auth': args.authentication}
-
-    if args.ssl:
-        st_cert=open(args.ca_cert_file, 'rt').read()
-        c=OpenSSL.crypto
-        ca_cert=c.load_certificate(c.FILETYPE_PEM, st_cert)
-        
-        st_cert=open(args.intermediate_cert_file, 'rt').read()
-        intermediate_cert=c.load_certificate(c.FILETYPE_PEM, st_cert)
-        
-        certs = OpenSSLCertificateAuthorities([ca_cert, intermediate_cert])
-        ssl_con = CertificateOptions(trustRoot=certs)
-
-        # now actually run a WAMP client using our session class ClientSession
-        runner = ApplicationRunner(url=args.url, realm=args.realm, extra=extra, ssl=ssl_con)
-
-    else:
-        # now actually run a WAMP client using our session class ClientSession
-        runner = ApplicationRunner(url=args.url, realm=args.realm, extra=extra)
-
-    runner.run(AppSession)
-
